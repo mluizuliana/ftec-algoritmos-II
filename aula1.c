baa96e4de1de03663928970c2fa31b6cb0dd0a24
Meigan Luiz Uliana <109111587+mluizuliana@users.noreply.github.com>
2023-10-13 14:23:12 -0300

    Add files via upload

---
+++ aula1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(void)
+{ 
+	int val1=10;
+	float val2=2.5;
+	char nome[20]="Meigan";
+	
+	printf("Eu amo C!");
+
+	printf("\n\nEu \namo\nC!");
+	
+	printf("\n\nEu \t\tamo C!");
+	
+	printf("\n\nEu \bamo C!");
+	
+	printf("\n\nEu amo \rC!");
+	
+	printf("\n\nEu amo C \\");
+
+	printf("\n\nEu amo C %%");
+
+	printf("\n\nValor da variavel 1: %d\n\nValor da variavel 2: %10.2f", val1,val2);
+	
+	printf("\n\nNome: %s", nome);
+
+	
+	return 0 ;
+
+}
