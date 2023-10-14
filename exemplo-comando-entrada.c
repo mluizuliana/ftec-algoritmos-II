8eb95e169b81141f9f87054b43f42ea02592c1b2
Meigan Luiz Uliana <109111587+mluizuliana@users.noreply.github.com>
2023-10-14 10:36:04 -0300

    Add files via upload

---
+++ exemplo-comando-entrada.c
@@ -0,0 +1,28 @@
+#include<stdio.h> 
+#include<locale.h> // colocar em português
+int main(void){
+
+	char sexo;
+	char nome[30];
+	float nota1,nota2;
+	
+	setlocale(LC_ALL,"portuguese"); // ou setlocale(0, "portuguese")
+	
+	printf("\nNome do aluno: ");
+	fflush(stdin);
+	gets(nome);
+	
+	printf("\nSexo (F/M): ");
+	fflush(stdin);
+	sexo=getchar();
+	
+	printf("\nInforme a nota 1: ");
+	scanf("%f", &nota1); // digitar com ponto, e não virgula 
+	printf("\nInforme a nota 2: ");
+	scanf("%f",&nota2);
+	
+	printf("\n%s é do sexo %c possui média = %.2f", nome, sexo,(nota1+nota2)/2);
+	
+	return 0;
+
+}
