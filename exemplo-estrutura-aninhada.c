53e2d6f858f124bbf6e2e3fd38352393027d4a2d
Meigan Luiz Uliana <109111587+mluizuliana@users.noreply.github.com>
2023-10-18 22:17:04 -0300

    Add files via upload

---
+++ exemplo-estrutura-aninhada.c
@@ -0,0 +1,14 @@
+if (<condicao1>) {
+    <instrucoes1>
+} else {
+    if (<condicao2>) {
+        <instrucoes2>
+    } else {
+        if (<condicao3>) {
+            <instrucoes3>
+        } else {
+            <instrucoes4>
+        }
+    }
+}
+
